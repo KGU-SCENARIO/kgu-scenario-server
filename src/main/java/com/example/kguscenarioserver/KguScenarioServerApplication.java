e10015d5fc8751d7efa99d7b0ed9e86cb15b4ecd
Minjoon Park <103498687+minjo-on@users.noreply.github.com>
2024-02-27 19:43:12 +0900

    Add files via upload

---
+++ src/main/java/com/example/kguscenarioserver/KguScenarioServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kguscenarioserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KguScenarioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KguScenarioServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kguscenarioserver/KguScenarioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kguscenarioserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KguScenarioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
