3b4c69f5dc9328d093795abc0a85ffb56439a1d2
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-03-11 14:33:54 +0900

    여러 요소 한번에 저장 기능 추가

--- src/main/java/com/example/kguscenarioserver/controller/ElementController.java
+++ src/main/java/com/example/kguscenarioserver/controller/ElementController.java
@@ -11,7 +11,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,25 +24,22 @@ public class ElementController {
 
     //element 저장
     @PostMapping("/save_element")
-    public void saveElement(@RequestBody @Valid ElementRequest request,
-                            HttpServletResponse response) throws IOException {
+    public void saveElement(@RequestBody @Valid ElementRequest request){
         elementService.saveElement(convertToElement(request));
-        response.sendRedirect("/scenario_list");
     }
 
+    //element-layer 여러개 한번에 저장
+    @PostMapping("/save_elements")
+    public void saveElementsLayer(@RequestBody @Valid List<ElementRequest> requests){
+        for(ElementRequest request : requests){
+            elementService.saveElement(convertToElement(request));
+        }
+    }
     //element 삭제
     @DeleteMapping("/delete_element/{id}")
-    public void deleteElement(@PathVariable Long id,
-                              HttpServletResponse response) throws IOException{
-        try{
-            Element element = elementService.getElement(id);
-            elementService.deleteElement(element);
-            response.sendRedirect("/scenario_list");
-        } catch (NoSuchElementException e){
-            response.sendError(response.SC_NOT_FOUND,"해당 시나리오가 없습니다.");
-        } catch (Exception e){
-            response.sendError(response.SC_INTERNAL_SERVER_ERROR,"에러가 발생했습니다.");
-        }
+    public void deleteElement(@PathVariable Long id){
+        Element element = elementService.getElement(id);
+        elementService.deleteElement(element);
     }
 
     //element 수정
@@ -53,13 +53,16 @@ public class ElementController {
     //dto -> entity 변환
     private Element convertToElement(ElementRequest request) {
         Element element = new Element();
+        element.setId(request.getId());
         element.setType(request.getType());
         element.setName(request.getName());
 
-        if(request.getType() != Type.LAYER){
-            element.setParent(elementService.getElement(request.getParentId()));
+        if(element.getType() != Type.LAYER){
+            Long parentId = request.getParentId();
+            element.setParent(elementService.getElement(parentId));
         }
 
         return element;
     }
+
 }
