779d7f1f03edd1622a904cfd78dba5a8d130732c
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-03-11 14:33:28 +0900

    Update ElementService.java

--- src/main/java/com/example/kguscenarioserver/service/ElementService.java
+++ src/main/java/com/example/kguscenarioserver/service/ElementService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,6 +24,11 @@ public class ElementService {
     @Transactional
     public void saveElement(Element saveElement){
         elementRepository.save(saveElement);
+    }
+
+    @Transactional
+    public void saveElements(List<Element> elements){
+        elementRepository.saveAll(elements);
     }
 
     //삭제 메소드
