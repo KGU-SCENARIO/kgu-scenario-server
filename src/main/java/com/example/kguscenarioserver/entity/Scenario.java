005cef7190948d765d2ccbb8bf9dd39604d1f985
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-05-01 06:02:39 +0900

    asdf

--- src/main/java/com/example/kguscenarioserver/entity/Scenario.java
+++ src/main/java/com/example/kguscenarioserver/entity/Scenario.java
@@ -14,13 +14,17 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Scenario {
+    //인덱스 번호
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "scenario_num")
     private Long id;
+    //특이사항 메모 공간
     private String memo;
+    //조합된 scenario 한줄로 저장
     @Column(columnDefinition = "TEXT")
     private String result;
+    //시나리오 생성된 시간
     @Column(name = "created_at")
     private Timestamp createdAt;
 
