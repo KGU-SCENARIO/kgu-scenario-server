09eba8b3afcc142539131fea3c2056f11ad2895e
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-06-03 22:14:07 +0900

    다 삭제

--- src/main/java/kguscenariobuilderserver/config/swagger/SwaggerConfig.java
+++ src/main/java/kguscenariobuilderserver/config/swagger/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package kguscenariobuilderserver.swagger;
+package kguscenariobuilderserver.config.swagger;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
--- src/main/java/kguscenariobuilderserver/repository/ScenarioRepository.java
+++ src/main/java/kguscenariobuilderserver/repository/ScenarioRepository.java
@@ -1,6 +1,6 @@
 package kguscenariobuilderserver.repository;
 
-import com.example.kguscenarioserver.entity.Scenario;
+import kguscenariobuilderserver.entity.Scenario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
