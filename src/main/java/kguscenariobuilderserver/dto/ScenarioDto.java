a1666ef9a03bb708db60d0f9d98b7a9c46c24351
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-06-07 15:34:18 +0900

    scenarioDto 생성

---
+++ src/main/java/kguscenariobuilderserver/dto/ScenarioDto.java
@@ -0,0 +1,16 @@
+package kguscenariobuilderserver.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ScenarioDto {
+    private Layer1Dto layer1Dto;
+    private Layer2Dto layer2Dto;
+    private Layer3Dto layer3Dto;
+    private Layer4Dto layer4Dto;
+    private Layer5Dto layer5Dto;
+    private Layer6Dto layer6Dto;
+    private Layer7Dto layer7Dto;
+}
