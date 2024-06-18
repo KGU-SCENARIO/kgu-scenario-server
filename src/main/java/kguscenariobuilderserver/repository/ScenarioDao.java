51c378c210df7d2e10342f50d29eb0b3e35317f9
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-06-18 15:58:11 +0900

    Revert "tc 룰셋 검증방식 구현"
    This reverts commit 8682a2624c4bda0877b0c4a53c034e322ec01ac5.

--- src/main/java/kguscenariobuilderserver/repository/ScenarioDao.java
+++ src/main/java/kguscenariobuilderserver/repository/ScenarioDao.java
@@ -2,7 +2,6 @@ package kguscenariobuilderserver.repository;
 
 import kguscenariobuilderserver.dto.InsertScenario;
 import kguscenariobuilderserver.dto.layer.*;
-import kguscenariobuilderserver.service.ValidationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,39 +11,15 @@ import java.lang.reflect.Field;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 @Repository
 @RequiredArgsConstructor
 public class ScenarioDAO {
     private final JdbcTemplate jdbcTemplate;
-    private final ValidationService validationService;
 
     public void batchInsertScenarios(InsertScenario insertScenario) {
-        Long maxId = findMaxTcId();
-
-        List<Integer> invalidIndexes = IntStream.range(0, insertScenario.getLayer1DTOs().size())
-                .parallel()
-                .filter(i -> !validationService.isValidLayer1(insertScenario.getLayer1DTOs().get(i))
-                        || !validationService.isValidLayer2(insertScenario.getLayer2DTOs().get(i))
-                        || !validationService.isValidLayer3(insertScenario.getLayer3DTOs().get(i))
-                        || !validationService.isValidLayer4(insertScenario.getLayer4DTOs().get(i))
-                        || !validationService.isValidLayer5(insertScenario.getLayer5DTOs().get(i)))
-                .boxed()
-                .collect(Collectors.toList());
-
-        removeInvalidIndexes(insertScenario.getLayer1DTOs(), invalidIndexes);
-        removeInvalidIndexes(insertScenario.getLayer2DTOs(), invalidIndexes);
-        removeInvalidIndexes(insertScenario.getLayer3DTOs(), invalidIndexes);
-        removeInvalidIndexes(insertScenario.getLayer4DTOs(), invalidIndexes);
-        removeInvalidIndexes(insertScenario.getLayer5DTOs(), invalidIndexes);
-        removeInvalidIndexes(insertScenario.getLayer6DTOs(), invalidIndexes);
-        removeInvalidIndexes(insertScenario.getLayer7DTOs(), invalidIndexes);
-
+        Long maxId = maxId();
         insertLayer1(insertScenario.getLayer1DTOs(), maxId);
         insertLayer2(insertScenario.getLayer2DTOs(), maxId);
         insertLayer3(insertScenario.getLayer3DTOs(), maxId);
@@ -52,19 +27,7 @@ public class ScenarioDAO {
         insertLayer5(insertScenario.getLayer5DTOs(), maxId);
         insertLayer6(insertScenario.getLayer6DTOs(), maxId);
         insertLayer7(insertScenario.getLayer7DTOs(), maxId);
-        insertScenario(insertScenario.getTc_description(), maxId, insertScenario.getLayer1DTOs().size());
-    }
-
-    private <T> void removeInvalidIndexes(List<T> list, List<Integer> invalidIndexes) {
-        Set<Integer> invalidIndexSet = new HashSet<>(invalidIndexes);
-
-        List<T> filteredList = IntStream.range(0, list.size())
-                .filter(i -> !invalidIndexSet.contains(i))
-                .mapToObj(list::get)
-                .toList();
-
-        list.clear();
-        list.addAll(filteredList);
+        insertScenario(insertScenario.getTc_description(), maxId);
     }
 
     private void executeBatchInsert(String sql, BatchPreparedStatementSetter pss) {
@@ -131,7 +94,7 @@ public class ScenarioDAO {
         });
     }
 
-    private void insertScenario(String tc_description, Long maxId,int tc_size) {
+    private void insertScenario(List<String> tc_description, Long maxId) {
         String sql = "insert into Scenario (scenario_id, layer1_id, layer2_id, layer3_id, layer4_id, layer5_id, layer6_id, layer7_id, tc_description, tc_create_at) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         executeBatchInsert(sql, new BatchPreparedStatementSetter() {
             public void setValues(PreparedStatement ps, int i) throws SQLException {
@@ -139,16 +102,16 @@ public class ScenarioDAO {
                 for (int j = 1; j <= 8; j++) {
                     ps.setLong(j, id);
                 }
-                ps.setString(9, tc_description);
+                ps.setString(9,tc_description.get(i));
                 ps.setTimestamp(10,new Timestamp(System.currentTimeMillis()));
             }
             public int getBatchSize() {
-                return tc_size;
+                return tc_description.size();
             }
         });
     }
 
-    private Long findMaxTcId() {
+    private Long maxId() {
         Long maxId = jdbcTemplate.queryForObject("SELECT MAX(id) FROM Layer7", Long.class);
         return (maxId == null) ? 0L : maxId;
     }
