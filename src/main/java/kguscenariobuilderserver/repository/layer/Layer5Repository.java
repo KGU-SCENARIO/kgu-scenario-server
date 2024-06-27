721a0ef81599f4e90bb9d350e37ce550668d31ae
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-06-27 23:28:12 +0900

    scenario 삭제 메소드 수정

---
+++ src/main/java/kguscenariobuilderserver/repository/layer/Layer5Repository.java
@@ -0,0 +1,7 @@
+package kguscenariobuilderserver.repository.layer;
+
+import kguscenariobuilderserver.entity.layer.Layer5;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Layer5Repository extends JpaRepository<Layer5, Long> {
+}
--- src/main/java/kguscenariobuilderserver/service/ScenarioService.java
+++ src/main/java/kguscenariobuilderserver/service/ScenarioService.java
@@ -2,9 +2,11 @@ package kguscenariobuilderserver.service;
 
 import kguscenariobuilderserver.dto.InsertScenario;
 import kguscenariobuilderserver.dto.ScenarioDTO;
+import kguscenariobuilderserver.entity.layer.Layer3;
 import kguscenariobuilderserver.exception.InsertScenarioException;
 import kguscenariobuilderserver.repository.ScenarioDAO;
 import kguscenariobuilderserver.repository.ScenarioRepository;
+import kguscenariobuilderserver.repository.layer.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
@@ -21,6 +23,14 @@ public class ScenarioService {
     private final ScenarioDAO scenarioDAO;
 
     private final ScenarioRepository scenarioRepository;
+    private final Layer1Repository layer1Repository;
+    private final Layer2Repository layer2Repository;
+    private final Layer3Repository layer3Repository;
+    private final Layer4Repository layer4Repository;
+    private final Layer5Repository layer5Repository;
+    private final Layer6Repository layer6Repository;
+    private final Layer7Repository layer7Repository;
+
 
     @Value("${page.size}") private int PAGE_SIZE;
 
@@ -39,7 +49,16 @@ public class ScenarioService {
 
     @Transactional
     public String deleteScenarios(){
-        scenarioRepository.deleteAll();
+
+        scenarioRepository.deleteAllInBatch();
+        layer1Repository.deleteAllInBatch();
+        layer2Repository.deleteAllInBatch();
+        layer3Repository.deleteAllInBatch();
+        layer4Repository.deleteAllInBatch();
+        layer5Repository.deleteAllInBatch();
+        layer6Repository.deleteAllInBatch();
+        layer7Repository.deleteAllInBatch();
+
         return "삭제 완료";
     }
 
