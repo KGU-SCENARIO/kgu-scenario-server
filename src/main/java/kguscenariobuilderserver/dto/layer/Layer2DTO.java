83207e41a40d04f1b4635949c529da36e4f1b33a
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-11-21 23:54:01 +0900

    refactor: layer2dto 수정

--- src/main/java/kguscenariobuilderserver/dto/layer/Layer2DTO.java
+++ src/main/java/kguscenariobuilderserver/dto/layer/Layer2DTO.java
@@ -1,91 +1,114 @@
 package kguscenariobuilderserver.dto.layer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import kguscenariobuilderserver.entity.layer.Layer2;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Builder
-public class Layer2DTO {
-    @JsonProperty("중앙분리대")
-    private String median_barrier;
+public record Layer2DTO(
+        @NotBlank
+        @JsonProperty("중앙분리대")
+        String median_barrier,
 
-    @JsonProperty("방호울타리")
-    private String guardrail;
+        @NotBlank
+        @JsonProperty("방호울타리")
+        String guardrail,
 
-    @JsonProperty("충격흡수시설")
-    private String impact_absorbing_facility;
+        @NotBlank
+        @JsonProperty("충격흡수시설")
+        String impact_absorbing_facility,
 
-    @JsonProperty("연속조명")
-    private String continuous_lighting;
+        @NotBlank
+        @JsonProperty("연속조명")
+        String continuous_lighting,
 
-    @JsonProperty("국부조명")
-    private String local_lighting;
+        @NotBlank
+        @JsonProperty("국부조명")
+        String local_lighting,
 
-    @JsonProperty("과속방지턱")
-    private String speed_bump;
+        @NotBlank
+        @JsonProperty("과속방지턱")
+        String speed_bump,
 
-    @JsonProperty("차량신호")
-    private String vehicle_signal;
+        @NotBlank
+        @JsonProperty("차량신호")
+        String vehicle_signal,
 
-    @JsonProperty("도로상태 예고")
-    private String road_condition_alert;
+        @NotBlank
+        @JsonProperty("도로상태 예고")
+        String road_condition_alert,
 
-    @JsonProperty("노면상황 예고")
-    private String pavement_condition_alert;
-    
-    @JsonProperty("기상상황 예고")
-    private String weather_condition_alert;
+        @NotBlank
+        @JsonProperty("노면상황 예고")
+        String pavement_condition_alert,
 
-    @JsonProperty("기타주의 예고")
-    private String other_warning_alerts;
+        @NotBlank
+        @JsonProperty("기상상황 예고")
+        String weather_condition_alert,
 
-    @JsonProperty("통행금지")
-    private String no_entry;
+        @NotBlank
+        @JsonProperty("기타주의 예고")
+        String other_warning_alerts,
 
-    @JsonProperty("통행제한")
-    private String restricted_access;
+        @NotBlank
+        @JsonProperty("통행금지")
+        String no_entry,
 
-    @JsonProperty("금지사항")
-    private String prohibited_items;
+        @NotBlank
+        @JsonProperty("통행제한")
+        String restricted_access,
 
-    @JsonProperty("도로지정")
-    private String road_designation;
+        @NotBlank
+        @JsonProperty("금지사항")
+        String prohibited_items,
 
-    @JsonProperty("통행방법")
-    private String traffic_method;
+        @NotBlank
+        @JsonProperty("도로지정")
+        String road_designation,
 
-    @JsonProperty("중앙선")
-    private String center_line;
+        @NotBlank
+        @JsonProperty("통행방법")
+        String traffic_method,
 
-    @JsonProperty("차선")
-    private String lane_line;
+        @NotBlank
+        @JsonProperty("중앙선")
+        String center_line,
 
-    @JsonProperty("사전예고 표시")
-    private String advance_warning_sign;
+        @NotBlank
+        @JsonProperty("차선")
+        String lane_line,
 
-    @JsonProperty("안전속도 표시")
-    private String safe_speed_sign;
+        @NotBlank
+        @JsonProperty("사전예고 표시")
+        String advance_warning_sign,
 
-    @JsonProperty("금지구역 표시")
-    private String no_access_sign;
+        @NotBlank
+        @JsonProperty("안전속도 표시")
+        String safe_speed_sign,
 
-    @JsonProperty("최고제한속도")
-    private int maximum_speed_limit;
+        @NotBlank
+        @JsonProperty("금지구역 표시")
+        String no_access_sign,
 
-    @JsonProperty("최저제한속도")
-    private String minimum_speed_limit;
+        @NotNull
+        @JsonProperty("최고제한속도")
+        int maximum_speed_limit,
 
-    @JsonProperty("보호구역")
-    private String protected_zone;
+        @NotBlank
+        @JsonProperty("최저제한속도")
+        String minimum_speed_limit,
 
-    @JsonProperty("전용도로")
-    private String exclusive_road;
+        @NotBlank
+        @JsonProperty("보호구역")
+        String protected_zone,
 
-    public static Layer2DTO toDTO(Layer2 layer2) {
+        @NotBlank
+        @JsonProperty("전용도로")
+        String exclusive_road
+) {
+    public static Layer2DTO from(Layer2 layer2) {
         return Layer2DTO.builder()
                 .median_barrier(layer2.getMedian_barrier())
                 .guardrail(layer2.getGuardrail())
