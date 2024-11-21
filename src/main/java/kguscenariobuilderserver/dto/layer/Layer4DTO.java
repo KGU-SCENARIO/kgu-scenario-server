cf32a8a823a1c0999f85acf58ae2d560a0012f09
minjo-on <103498687+minjo-on@users.noreply.github.com>
2024-11-22 00:03:02 +0900

    refactor: layer4dto 수정

--- src/main/java/kguscenariobuilderserver/dto/layer/Layer4DTO.java
+++ src/main/java/kguscenariobuilderserver/dto/layer/Layer4DTO.java
@@ -1,6 +1,8 @@
 package kguscenariobuilderserver.dto.layer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import kguscenariobuilderserver.entity.layer.layer4.Layer4;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,126 +11,205 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
-public class Layer4DTO {
+public record Layer4DTO (
+         /*
 
-    /*
+        NPC 1
 
-    NPC 1
+        */
+        @NotBlank
+        @JsonProperty("NPC1 객체종류")
+        String npc1_type,
 
-    */
-    @JsonProperty("NPC1 객체종류")
-    private String npc1_type;
-    @JsonProperty("NPC1 객체위치-차로")
-    private int npc1_position_lane;
-    @JsonProperty("NPC1 객체위치-상대거리")
-    private String npc1_relative_distance;
-    @JsonProperty("NPC1 객체속도")
-    private String npc1_speed;
-    @JsonProperty("NPC1 객체가감속도")
-    private String npc1_acceleration;
-    @JsonProperty("NPC1 Trigger(가감속)")
-    private String npc1_trigger_acceleration;
-    @JsonProperty("NPC1 객체행동(시작)")
-    private String npc1_action_start;
-    @JsonProperty("NPC1 객체행동(동작)")
-    private String npc1_action_perform;
-    @JsonProperty("NPC1 Trigger(동작)")
-    private String npc1_trigger_action;
+        @NotNull
+        @JsonProperty("NPC1 객체위치-차로")
+        int npc1_position_lane,
 
-    /*
+        @NotBlank
+        @JsonProperty("NPC1 객체위치-상대거리")
+        String npc1_relative_distance,
 
-    NPC 2
+        @NotBlank
+        @JsonProperty("NPC1 객체속도")
+        String npc1_speed,
 
-    */
-    @JsonProperty("NPC2 객체종류")
-    private String npc2_type;
-    @JsonProperty("NPC2 객체위치-차로")
-    private int npc2_position_lane;
-    @JsonProperty("NPC2 객체위치-상대거리")
-    private String npc2_relative_distance;
-    @JsonProperty("NPC2 객체속도")
-    private String npc2_speed;
-    @JsonProperty("NPC2 객체가감속도")
-    private String npc2_acceleration;
-    @JsonProperty("NPC2 Trigger(가감속)")
-    private String npc2_trigger_acceleration;
-    @JsonProperty("NPC2 객체행동(시작)")
-    private String npc2_action_start;
-    @JsonProperty("NPC2 객체행동(동작)")
-    private String npc2_action_perform;
-    @JsonProperty("NPC2 Trigger(동작)")
-    private String npc2_trigger_action;
+        @NotBlank
+        @JsonProperty("NPC1 객체가감속도")
+        String npc1_acceleration,
 
-    /*
+        @NotBlank
+        @JsonProperty("NPC1 Trigger(가감속)")
+        String npc1_trigger_acceleration,
 
-    NPC 3
+        @NotBlank
+        @JsonProperty("NPC1 객체행동(시작)")
+        String npc1_action_start,
 
-    */
-    @JsonProperty("NPC3 객체종류")
-    private String npc3_type;
-    @JsonProperty("NPC3 객체위치-차로")
-    private int npc3_position_lane;
-    @JsonProperty("NPC3 객체위치-상대거리")
-    private String npc3_relative_distance;
-    @JsonProperty("NPC3 객체속도")
-    private String npc3_speed;
-    @JsonProperty("NPC3 객체가감속도")
-    private String npc3_acceleration;
-    @JsonProperty("NPC3 Trigger(가감속)")
-    private String npc3_trigger_acceleration;
-    @JsonProperty("NPC3 객체행동(시작)")
-    private String npc3_action_start;
-    @JsonProperty("NPC3 객체행동(동작)")
-    private String npc3_action_perform;
-    @JsonProperty("NPC3 Trigger(동작)")
-    private String npc3_trigger_action;
+        @NotBlank
+        @JsonProperty("NPC1 객체행동(동작)")
+        String npc1_action_perform,
 
-    /*
+        @NotBlank
+        @JsonProperty("NPC1 Trigger(동작)")
+        String npc1_trigger_action,
 
-    NPC 4
+        /*
 
-    */
-    @JsonProperty("NPC4 객체종류")
-    private String npc4_type;
-    @JsonProperty("NPC4 객체위치-차로")
-    private int npc4_position_lane;
-    @JsonProperty("NPC4 객체위치-상대거리")
-    private String npc4_relative_distance;
-    @JsonProperty("NPC4 객체속도")
-    private String npc4_speed;
-    @JsonProperty("NPC4 객체가감속도")
-    private String npc4_acceleration;
-    @JsonProperty("NPC4 Trigger(가감속)")
-    private String npc4_trigger_acceleration;
-    @JsonProperty("NPC4 객체행동(시작)")
-    private String npc4_action_start;
-    @JsonProperty("NPC4 객체행동(동작)")
-    private String npc4_action_perform;
-    @JsonProperty("NPC4 Trigger(동작)")
-    private String npc4_trigger_action;
+        NPC 2
+
+        */
+        @NotBlank
+        @JsonProperty("NPC2 객체종류")
+        String npc2_type,
+
+        @NotNull
+        @JsonProperty("NPC2 객체위치-차로")
+        int npc2_position_lane,
+
+        @NotBlank
+        @JsonProperty("NPC2 객체위치-상대거리")
+        String npc2_relative_distance,
+
+        @NotBlank
+        @JsonProperty("NPC2 객체속도")
+        String npc2_speed,
+
+        @NotBlank
+        @JsonProperty("NPC2 객체가감속도")
+        String npc2_acceleration,
+
+        @NotBlank
+        @JsonProperty("NPC2 Trigger(가감속)")
+        String npc2_trigger_acceleration,
+
+        @NotBlank
+        @JsonProperty("NPC2 객체행동(시작)")
+        String npc2_action_start,
+
+        @NotBlank
+        @JsonProperty("NPC2 객체행동(동작)")
+        String npc2_action_perform,
+
+        @NotBlank
+        @JsonProperty("NPC2 Trigger(동작)")
+        String npc2_trigger_action,
+
+        /*
+
+        NPC 3
+
+        */
+
+        @NotBlank
+        @JsonProperty("NPC3 객체종류")
+        String npc3_type,
+
+        @NotNull
+        @JsonProperty("NPC3 객체위치-차로")
+        int npc3_position_lane,
+
+        @NotBlank
+        @JsonProperty("NPC3 객체위치-상대거리")
+        String npc3_relative_distance,
+
+        @NotBlank
+        @JsonProperty("NPC3 객체속도")
+        String npc3_speed,
+
+        @NotBlank
+        @JsonProperty("NPC3 객체가감속도")
+        String npc3_acceleration,
+
+        @NotBlank
+        @JsonProperty("NPC3 Trigger(가감속)")
+        String npc3_trigger_acceleration,
+
+        @NotBlank
+        @JsonProperty("NPC3 객체행동(시작)")
+        String npc3_action_start,
+
+        @NotBlank
+        @JsonProperty("NPC3 객체행동(동작)")
+        String npc3_action_perform,
+
+        @NotBlank
+        @JsonProperty("NPC3 Trigger(동작)")
+        String npc3_trigger_action,
+
+        /*
+
+        NPC 4
+
+        */
+
+        @NotBlank
+        @JsonProperty("NPC4 객체종류")
+        String npc4_type,
+
+        @NotNull
+        @JsonProperty("NPC4 객체위치-차로")
+        int npc4_position_lane,
+
+        @NotBlank
+        @JsonProperty("NPC4 객체위치-상대거리")
+        String npc4_relative_distance,
+
+        @NotBlank
+        @JsonProperty("NPC4 객체속도")
+        String npc4_speed,
+
+        @NotBlank
+        @JsonProperty("NPC4 객체가감속도")
+        String npc4_acceleration,
+
+        @NotBlank
+        @JsonProperty("NPC4 Trigger(가감속)")
+        String npc4_trigger_acceleration,
+
+        @NotBlank
+        @JsonProperty("NPC4 객체행동(시작)")
+        String npc4_action_start,
+
+        @NotBlank
+        @JsonProperty("NPC4 객체행동(동작)")
+        String npc4_action_perform,
+
+        @NotBlank
+        @JsonProperty("NPC4 Trigger(동작)")
+        String npc4_trigger_action,
 
 
-    @JsonProperty("교통상황(LOS)")
-    private String traffic_condition_LOS;
+        @NotBlank
+        @JsonProperty("교통상황(LOS)")
+        String traffic_condition_LOS,
 
-    /*
+        /*
 
-    자율 주행차
+        자율 주행차
 
-    */
-    @JsonProperty("자율주행차 객체종류")
-    private String av_type;
-    @JsonProperty("자율주행차 객체위치-차로")
-    private int av_lane_position;
-    @JsonProperty("자율주행차 객체속도")
-    private String av_speed;
-    @JsonProperty("자율주행차 객체 행동(시작)")
-    private String av_action_start;
-    @JsonProperty("자율주행차 객체 예상 행동(동작)")
-    private String av_action_perform;
+        */
 
+        @NotBlank
+        @JsonProperty("자율주행차 객체종류")
+        String av_type,
 
-    public static Layer4DTO toDTO(Layer4 layer4) {
+        @NotNull
+        @JsonProperty("자율주행차 객체위치-차로")
+        int av_lane_position,
+
+        @NotBlank
+        @JsonProperty("자율주행차 객체속도")
+        String av_speed,
+
+        @NotBlank
+        @JsonProperty("자율주행차 객체 행동(시작)")
+        String av_action_start,
+
+        @NotBlank
+        @JsonProperty("자율주행차 객체 예상 행동(동작)")
+        String av_action_perform
+) {
+    public static Layer4DTO from(Layer4 layer4) {
         if (layer4 == null) {
             return null;
         }
